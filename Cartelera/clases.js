2928038546d1d363e5ed8f7ef1936a204322ffa9
FrancoPanfilo <108556532+FrancoPanfilo@users.noreply.github.com>
2022-08-30 22:29:25 -0300

    una fila , arreglar imagenes faltantes

--- Cartelera/clases.js
+++ Cartelera/clases.js
@@ -1,8 +1,46 @@
-let obtenerCartelera = async ()=>{
-    await fetch("pelis.json")
-    .then((res)=>res= res.json())
-    .then((res)=>res=res)
+let getJSONData = function (url) {
+    let result = {};
+    return fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
+        })
+        .then(function (response) {
+            result.status = 'ok';
+            result.data = response;
+            return result;
+        })
+        .catch(function (error) {
+            result.status = 'error';
+            result.data = error;
+            return result;
+        });
 }
-let data
-obtenerCartelera()
-console.log(data)
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData("https://francopanfilo.github.io/pelis.json").then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            pelisArray = resultObj.data;
+        }
+        let nuevaFila = document.createElement("tr")
+        nuevaFila.innerHTML = ` `
+        for (let i = 0; i < pelisArray.movies.length; i++) {
+            let mostrar = pelisArray.movies[i]
+            let peli = document.createElement("td")
+            peli.innerHTML = `
+            <td>
+            <article>
+            <h5>${mostrar.title}</h5>
+            <img src="${mostrar.posterUrl}">
+            <p class="año">${mostrar.year}</p>
+            </article></td>`
+
+            nuevaFila.appendChild(peli)
+        }
+        let tabla= document.getElementById("cuerpo")
+        tabla.appendChild(nuevaFila)
+    });
+});
